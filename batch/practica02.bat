49f8d7503bc66724b8afc97d1fab078074a85a52
Raul Jonatan <rauljhonatan.tola@gmail.com>
2015-05-08 07:52:10 -0500

    Archivo batch went Funcionando

---
+++ batch/practica02.bat
@@ -0,0 +1,8 @@
+@echo off
+::Programa que Determine si un numero ingresado es PAR  o IMPAR
+
+::ENTRADA
+set /p "numero=Ingrese Numero: " 
+::PROCESO
+::SALIDA
+echo El numero es: %numero%
--- batch/went.bat
+++ batch/went.bat
@@ -1,9 +1,18 @@
 @echo off
-title DHCP-Automatico / Desactivar-PROXY
-::Programa Script para Desactivar proxy-> IE ; y restaura confoguracion de IPV4
+
 REM  Realizado por @rjotac
 
+
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v ProxyEnable /t REG_DWORD /d 0 /f
+
+set SUBKEY=Software\Microsoft\Windows\CurrentVersion\Internet Settings
+
+reg delete "HKCU\%SUBKEY%\Connections" /f
+reg add    "HKCU\%SUBKEY%" /f /v MigrateProxy /t REG_DWORD /d 0x1
+reg add    "HKCU\%SUBKEY%" /f /v ProxyEnable /t REG_DWORD /d 0x0
+reg delete "HKCU\%SUBKEY%" /f /v ProxyServer
+reg delete "HKCU\%SUBKEY%" /f /v ProxyOverride
+
 
 ipconfig /flushdns
 
@@ -15,6 +24,6 @@ netsh interface ipv4 set address name="ethernet" source=dhcp
 
 netsh interface ipv4 set dnsservers name="ethernet" source=dhcp
 
-mshta "javascript:var sh=new ActiveXObject( 'WScript.Shell' ); sh.Popup( 'Cambios Realizados Con Éxito!', 10, 'Servicios Computacionales', 64 );close()"
+mshta "javascript:var sh=new ActiveXObject( 'WScript.Shell' ); sh.Popup( 'Cambios Realizados Con Exito!', 10, 'Servicios Computacionales', 64 );close()"
 
 exit
