0e9ae39639f9593a4c2d52c5d1afee317dae7b34
Raul Jonatan <rauljhonatan.tola@gmail.com>
2015-05-08 08:03:09 -0500

    ejemplos de clase 4

---
+++ python/listas/ejemplo02.py
@@ -0,0 +1,13 @@
+#Programa para  hallar el promedio de n notas insertadas (listas)
+
+#ENTRADA
+tam = input("Ingrese Cantidad de Notas: ")
+notas = []
+for i in range(tam):
+    nota = input("Valor: ")
+    notas.append(nota)
+
+#PROCESO
+#SALIDA
+
+print notas
