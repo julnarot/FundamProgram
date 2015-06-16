1671d7f787017aac68565d0ea3280f0be42380e4
Raul Jonatan <rauljhonatan.tola@gmail.com>
2015-06-16 17:27:39 -0500

    Juego 3 en raya en proceso

--- python/matrices/mat06.py
+++ python/matrices/mat06.py
@@ -2,15 +2,14 @@
 
 def crear_matriz(f,c):
     mat = []
-    for i in range(fila):
-        mat.append([0]*columna)
+    for i in range(f):
+        mat.append(['_']*c)
     return mat
 
 
 def reportar_matriz(mat,f):   
     for i in range(f):
-        print mat[i]
-
+        print mat[i]   
 
 def ingresar_matriz(mat,f,c):
     for i in range(f):
@@ -19,23 +18,39 @@ def ingresar_matriz(mat,f,c):
 	    mat[i][j]=data
 
 def ingresar_posicion(mat):
-    x = input("ubique columna: ")
-    y = input("ubique fila: ")
+    x = input("ubique    Fila: ")
+    y = input("ubique Columna: ")
     data = raw_input("Ingrese Ficha: ")
     mat[x][y] = data
+    
+def evaluar_ganador(mat):
+    for i in range(3):
+
+        if mat[i] == ['x','x','x']:
+            print "Ganastes"
+	else:
+	    print "sigue"
 
 fila = 3
 columna = 3
 
-estado = 10
-while != 0:
-    
-
-
 
 mat1 = crear_matriz(fila, columna)
 reportar_matriz(mat1, fila)
-ingresar_matriz(mat1, fila, columna)
-reportar_matriz(mat1, fila)
-ingresar_posicion(mat1)
-reportar_matriz(mat1,fila)
+std = 1
+
+#print "Jugador 1:"
+#ingresar_posicion(mat1)
+#reportar_matriz(mat1,fila)
+
+#print "Jugador 2"
+#ingresar_posicion(mat1)
+#reportar_matriz(mat1,fila)
+
+while std !=0:
+    
+    ingresar_posicion(mat1)
+    reportar_matriz(mat1,fila)
+    evaluar_ganador(mat1)
+    std = input("continuar? [1/0] :")
+
